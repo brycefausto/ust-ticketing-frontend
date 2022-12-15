35af13a9780b63d8b86cbd6fe00db8e0b1bbe214
Bryce <bryce_fausto@yahoo.com>
2022-12-16 05:01:02 +0800

    Fixed axios and chart.js bug

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import "styles/globals.css";
 
-axios.defaults.baseURL = process.env.NEXT_PUBLIC_BACKEND_URL;
+axios.defaults.baseURL = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:5000';
 
 const MyApp = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
